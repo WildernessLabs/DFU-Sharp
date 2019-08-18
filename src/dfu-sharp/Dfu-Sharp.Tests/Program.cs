918dcf9aadfa9c2146a87a552f2476810a268b50
Bryan Costanich <b@wowzer.net>
2019-08-18 11:43:47 -0700

    Converted to .csproj from .json

---
+++ src/dfu-sharp/Dfu-Sharp.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dfu_Sharp.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
